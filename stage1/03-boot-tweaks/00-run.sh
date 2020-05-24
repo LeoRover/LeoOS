848422bb019106c102df2e4298034164fe1477d0
Błażej Sowa <bsowa123@gmail.com>
2020-05-24 22:53:41 +0200

    expand root filesystem at first boot

--- stage1/03-boot-tweaks/00-run.sh
+++ stage1/03-boot-tweaks/00-run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 
+install -v -m 774 files/expand-rootfs "${ROOTFS_DIR}/sbin/"
+install -v -m 774 files/init_expand "${ROOTFS_DIR}/sbin/"
+
+install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
+
 log "Purging cloud-init"
 
-# Purge cloud-init
-rm -v -rf "${ROOTFS_DIR}/etc/cloud"
-rm -v -f "${ROOTFS_DIR}/boot/firmware/meta-data"
-rm -v -f "${ROOTFS_DIR}/boot/firmware/network-config"
-rm -v -f "${ROOTFS_DIR}/boot/firmware/user-data"
+rm -rf "${ROOTFS_DIR}/etc/cloud"
+rm -f "${ROOTFS_DIR}/boot/firmware/meta-data"
+rm -f "${ROOTFS_DIR}/boot/firmware/network-config"
+rm -f "${ROOTFS_DIR}/boot/firmware/user-data"
 
 on_chroot << EOF
 apt-get purge cloud-init -y
--- stage1/03-boot-tweaks/01-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-log "Configuring fstab"
-
-install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
