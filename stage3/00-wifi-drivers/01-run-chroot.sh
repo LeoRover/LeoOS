f5af05b33c23ee5f3e02272c13c40fdf16220707
Błażej Sowa <bsowa123@gmail.com>
2020-06-01 00:28:01 +0200

    install rtl8812au wifi drivers

---
+++ stage3/00-wifi-drivers/01-run-chroot.sh
@@ -0,0 +1,16 @@
+DRIVER_NAME=rtl8812au
+DRIVER_VERSION=5.7.0
+KERNEL_VERSION=$(dpkg-query -W -f='${binary:Package}\n' linux-image-* | head -n 1 | sed 's/linux-image-//')
+
+cd /tmp
+git clone -b v${DRIVER_VERSION} https://github.com/aircrack-ng/rtl8812au.git
+cd rtl8812au/
+
+sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
+sed -i 's/CONFIG_PLATFORM_ARM_RPI = n/CONFIG_PLATFORM_ARM_RPI = y/g' Makefile
+
+cp -r $(pwd) /usr/src/${DRIVER_NAME}-${DRIVER_VERSION}
+
+dkms add -m ${DRIVER_NAME} -v ${DRIVER_VERSION} -k ${KERNEL_VERSION}
+dkms build -m ${DRIVER_NAME} -v ${DRIVER_VERSION} -k ${KERNEL_VERSION}
+dkms install -m ${DRIVER_NAME} -v ${DRIVER_VERSION} -k ${KERNEL_VERSION}
---
+++ stage3/00-wifi-drivers/02-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+rm -rf "${ROOTFS_DIR}/tmp/rtl8812au"
