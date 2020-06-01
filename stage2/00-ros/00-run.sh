0474a977f2009a0054f70a5eead3b70a0c93fc67
Błażej Sowa <bsowa123@gmail.com>
2020-06-02 00:30:26 +0200

    ros.asc -> ros.key

--- stage2/00-ros/00-run.sh
+++ stage2/00-ros/00-run.sh
@@ -2,7 +2,7 @@
 
 install -v -m 664 files/ros-latest.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
-on_chroot apt-key add - < files/ros.asc
+on_chroot apt-key add - < files/ros.key
 
 on_chroot << EOF
 apt-get update
