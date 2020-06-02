72864dd16986098f58a4256dae5020ca1e825825
Błażej Sowa <bsowa123@gmail.com>
2020-06-02 19:47:00 +0200

    add execute permissions to leo-start and leo-stop scripts

--- stage3/02-leo-service/00-run.sh
+++ stage3/02-leo-service/00-run.sh
@@ -6,8 +6,8 @@ mkdir -p -m 777 "${ROOTFS_DIR}/var/log/ros/"
 install -v -m 644 files/setup.bash "${ROOTFS_DIR}/etc/ros/"
 install -v -m 644 files/robot.launch "${ROOTFS_DIR}/etc/ros/"
 install -v -m 644 files/robot.urdf.xacro "${ROOTFS_DIR}/etc/ros/urdf/"
-install -v -m 644 files/leo-start "${ROOTFS_DIR}/usr/sbin/"
-install -v -m 644 files/leo-stop "${ROOTFS_DIR}/usr/sbin/"
+install -v -m 755 files/leo-start "${ROOTFS_DIR}/usr/sbin/"
+install -v -m 755 files/leo-stop "${ROOTFS_DIR}/usr/sbin/"
 install -v -m 644 files/leo.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 on_chroot << EOF
