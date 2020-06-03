c3054103cedf19fd5084d13a5bd580bd100a5645
Błażej Sowa <bsowa123@gmail.com>
2020-06-03 20:18:42 +0200

    stage1 refactor

--- stage1/01-packages/01-run.sh
+++ stage1/01-packages/01-run.sh
@@ -4,4 +4,5 @@ rm -v -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/01autoremove-kernels"
 
 on_chroot << EOF
 apt autoremove --purge -y
+apt-get remove open-iscsi -y
 EOF
--- stage1/05-net-tweaks/02-run.sh
+++ stage1/05-net-tweaks/02-run.sh
@@ -3,7 +3,10 @@
 install -v -m 644 files/01-network-manager-all.yaml "${ROOTFS_DIR}/etc/netplan/"
 
 echo "${TARGET_HOSTNAME}" > "${ROOTFS_DIR}/etc/hostname"
-echo "127.0.0.1 ${TARGET_HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
+
+if ! grep -q "127.0.0.1 ${TARGET_HOSTNAME}" "${ROOTFS_DIR}/etc/hosts"; then
+    echo "127.0.0.1 ${TARGET_HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
+fi
 
 install -v -m 644 files/regenerate_ssh_host_keys.service "${ROOTFS_DIR}/etc/systemd/system/"
 
