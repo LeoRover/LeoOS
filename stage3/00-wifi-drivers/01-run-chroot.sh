516bfd30c95a02f0782286e1fa437de5ebe36f53
Błażej Sowa <bsowa123@gmail.com>
2020-06-03 15:16:37 +0200

    freeze rtl8812au wifi driver version to specific commit sha

--- stage3/00-wifi-drivers/01-run-chroot.sh
+++ stage3/00-wifi-drivers/01-run-chroot.sh
@@ -1,10 +1,12 @@
 DRIVER_NAME=rtl8812au
 DRIVER_VERSION=5.7.0
 KERNEL_VERSION=$(dpkg-query -W -f='${binary:Package}\n' linux-image-* | head -n 1 | sed 's/linux-image-//')
+COMMIT_SHA=76c3cf81fdc71af4338571b6404beb2ba3835a62
 
 cd /tmp
-git clone -b v${DRIVER_VERSION} https://github.com/aircrack-ng/rtl8812au.git
+git clone -n https://github.com/aircrack-ng/rtl8812au.git
 cd rtl8812au/
+git checkout ${COMMIT_SHA}
 
 sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
 sed -i 's/CONFIG_PLATFORM_ARM_RPI = n/CONFIG_PLATFORM_ARM_RPI = y/g' Makefile
