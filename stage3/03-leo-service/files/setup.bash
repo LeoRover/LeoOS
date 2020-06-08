a7ffec946aac36b084625fcc5bb480d17e5d14ae
Błażej Sowa <bsowa123@gmail.com>
2020-06-08 21:27:58 +0200

    update leo service files

--- stage3/03-leo-service/files/setup.bash
+++ stage3/03-leo-service/files/setup.bash
@@ -4,17 +4,19 @@ source /opt/ros/melodic/setup.bash
 
 # Path to the launch file to start
 export LAUNCH_FILE="/etc/ros/robot.launch"
+# Additional command-line arguments passed to roslaunch
+#export ROSLAUNCH_ARGS="--wait"
 
 ## leo_bringup variables
 
 # Uncomment this line if you use the Pololu motors
-# export LEO_MOTORS=pololu
+#export LEO_MOTORS=pololu
 
-## ROS variables
+## ROS Environment Variables
 ## http://wiki.ros.org/ROS/EnvironmentVariables
 
 export ROS_LOG_DIR=/var/log/ros
-export ROS_HOSTNAME=leo
-# export ROS_IP=10.0.0.1
+export ROS_HOSTNAME=master.localnet
 export ROS_MASTER_URI=http://localhost:11311
-# export ROS_NAMESPACE=leo1
+#export ROS_IP=10.0.0.1
+#export ROS_NAMESPACE=leo1
