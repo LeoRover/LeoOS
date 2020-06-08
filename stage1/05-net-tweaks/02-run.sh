cbcd7bcde5a8a26f2ed4246b390f4cf24657a7d5
Błażej Sowa <bsowa123@gmail.com>
2020-06-09 01:29:20 +0200

    add network manager polkit local authority rule

--- stage1/05-net-tweaks/02-run.sh
+++ stage1/05-net-tweaks/02-run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
-install -v -m 644 files/01-network-manager-all.yaml "${ROOTFS_DIR}/etc/netplan/"
+install -v -m 644 files/netplan/*.yaml "${ROOTFS_DIR}/etc/netplan/"
+install -v -m 644 files/polkit/*.pkla "${ROOTFS_DIR}/etc/polkit-1/localauthority/50-local.d/"
 
 echo "${TARGET_HOSTNAME}" > "${ROOTFS_DIR}/etc/hostname"
 
 if ! grep -q "127.0.0.1 ${TARGET_HOSTNAME}" "${ROOTFS_DIR}/etc/hosts"; then
-    echo "127.0.0.1 ${TARGET_HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
+    printf "\n127.0.0.1 ${TARGET_HOSTNAME}\n" >> "${ROOTFS_DIR}/etc/hosts"
 fi
 
 install -v -m 644 files/regenerate_ssh_host_keys.service "${ROOTFS_DIR}/etc/systemd/system/"
