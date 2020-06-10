86959d724c6bc34a3bfbc16464c922a90a6dc142
Błażej Sowa <bsowa123@gmail.com>
2020-06-11 00:17:04 +0200

    export FK_MACHINE variable, so flash-kernel does not fail

--- stage1/01-packages/01-run.sh
+++ stage1/01-packages/01-run.sh
@@ -3,6 +3,7 @@
 rm -v -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/01autoremove-kernels"
 
 on_chroot << EOF
+export FK_MACHINE="Raspberry Pi 4 Model B"
 apt autoremove --purge -y
 apt-get remove open-iscsi -y
 EOF
