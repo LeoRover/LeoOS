e11029a762b37e1fcdf3c4520cbf2eed367cb942
Błażej Sowa <bsowa123@gmail.com>
2020-06-11 00:18:38 +0200

    install leo_ui from apt repository

--- stage3/04-leo-ui/01-run.sh
+++ stage3/04-leo-ui/01-run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-if [ ! -d /opt/leo_ui ]; then
-    git clone https://github.com/LeoRover/leo_ui.git /opt/leo_ui
-fi
-EOF
-
 install -v -m 644 files/leo_ui "${ROOTFS_DIR}/etc/nginx/sites-available/"
 rm -v -f "${ROOTFS_DIR}/etc/nginx/sites-enabled/default"
 ln -fs "/etc/nginx/sites-available/leo_ui" "${ROOTFS_DIR}/etc/nginx/sites-enabled/leo_ui"
