5b48bec1d82beb3c8a842f53f23c5f736fb67bd2
Błażej Sowa <bsowa123@gmail.com>
2020-06-15 20:22:24 +0200

    Add scripts for enabling and disabling Access Point

--- stage3/01-net-config/02-run.sh
+++ stage3/01-net-config/02-run.sh
@@ -7,6 +7,7 @@ install -v -m 644 files/dnsmasq.service.d/*.conf "${ROOTFS_DIR}/etc/systemd/syst
 mkdir -p -m 755 "${ROOTFS_DIR}/etc/systemd/system/hostapd.service.d"
 install -v -m 644 files/hostapd.service.d/*.conf "${ROOTFS_DIR}/etc/systemd/system/hostapd.service.d/"
 install -v -m 755 files/firstboot.d/* "${ROOTFS_DIR}/etc/firstboot.d/"
+install -v -m 755 files/scripts/* "${ROOTFS_DIR}/usr/sbin/"
 
 install -v -m 644 files/iptables.ipv4.nat "${ROOTFS_DIR}/etc/iptables.ipv4.nat"
 install -v -m 755 files/rc.local "${ROOTFS_DIR}/etc/rc.local"
