aefb540b0b67a5fd20cf733234009995a0f33b56
Błażej Sowa <bsowa123@gmail.com>
2020-06-15 20:18:43 +0200

    Purge all cloud packages

--- stage1/01-packages/01-run.sh
+++ stage1/01-packages/01-run.sh
@@ -4,6 +4,6 @@ rm -v -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/01autoremove-kernels"
 
 on_chroot << EOF
 export FK_MACHINE="Raspberry Pi 4 Model B"
+apt-get purge cloud-init cloud-guest-utils cloud-initramfs-copymods cloud-initramfs-dyn-netconf open-iscsi -y
 apt autoremove --purge -y
-apt-get remove open-iscsi -y
 EOF
--- stage1/03-boot-tweaks/01-run.sh
+++ stage1/03-boot-tweaks/01-run.sh
@@ -6,13 +6,7 @@ install -v -m 755 files/init_firstboot "${ROOTFS_DIR}/sbin/"
 
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
-log "Purging cloud-init"
-
 rm -rf "${ROOTFS_DIR}/etc/cloud"
 rm -f "${ROOTFS_DIR}/boot/firmware/meta-data"
 rm -f "${ROOTFS_DIR}/boot/firmware/network-config"
 rm -f "${ROOTFS_DIR}/boot/firmware/user-data"
-
-on_chroot << EOF
-apt-get purge cloud-init -y
-EOF
