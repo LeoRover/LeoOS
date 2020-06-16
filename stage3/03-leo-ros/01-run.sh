98654e9f0bf512df9f8525071e325d4362968435
Błażej Sowa <bsowa123@gmail.com>
2020-06-16 17:36:26 +0200

    Add ros group, change /etc/ros files owners and permissions

--- stage3/03-leo-ros/01-run.sh
+++ stage3/03-leo-ros/01-run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -e
 
-mkdir -p "${ROOTFS_DIR}/etc/ros/urdf"
-mkdir -p -m 777 "${ROOTFS_DIR}/var/log/ros/"
+mkdir -p -m 775 "${ROOTFS_DIR}/etc/ros/urdf"
+mkdir -p -m 775 "${ROOTFS_DIR}/var/log/ros/"
 
-install -v -m 644 files/setup.bash "${ROOTFS_DIR}/etc/ros/"
-install -v -m 644 files/robot.launch "${ROOTFS_DIR}/etc/ros/"
-install -v -m 644 files/robot.urdf.xacro "${ROOTFS_DIR}/etc/ros/urdf/"
-install -v -m 755 files/leo-start "${ROOTFS_DIR}/usr/sbin/"
-install -v -m 755 files/leo-stop "${ROOTFS_DIR}/usr/sbin/"
+install -v -m 664 files/setup.bash "${ROOTFS_DIR}/etc/ros/"
+install -v -m 664 files/robot.launch "${ROOTFS_DIR}/etc/ros/"
+install -v -m 664 files/robot.urdf.xacro "${ROOTFS_DIR}/etc/ros/urdf/"
+install -v -m 755 files/leo-start "${ROOTFS_DIR}/usr/bin/"
+install -v -m 755 files/leo-stop "${ROOTFS_DIR}/usr/bin/"
 install -v -m 644 files/leo.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 on_chroot << EOF
+groupadd -f -r ros
+adduser $FIRST_USER_NAME ros
+chown root:ros -R "/etc/ros"
+chown root:ros "/var/log/ros"
 systemctl enable leo
 EOF
 
