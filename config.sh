2a6d26447581e518104071ad3fcc24fd38dcca83
Błażej Sowa <bsowa123@gmail.com>
2020-07-15 14:31:01 +0200

    Export config variables

--- config.sh
+++ config.sh
@@ -1,13 +1,16 @@
 IMG_NAME="LeoOS"
 IMG_VERSION="0.1"
 
-BASE_IMG_URL=http://cdimage.ubuntu.com/releases/18.04.4/release/ubuntu-18.04.4-preinstalled-server-armhf+raspi3.img.xz
+export GIT_REPO="https://github.com/LeoRover/leo_os"
+export GIT_HASH="$(git rev-parse HEAD)"
 
-TARGET_HOSTNAME="leo"
-FIRST_USER_NAME="pi"
-FIRST_USER_PASS="raspberry"
-
-LOCALE_DEFAULT="en_GB.UTF-8"
-KEYBOARD_KEYMAP="gb"
-KEYBOARD_LAYOUT="English (UK)"
-TIMEZONE_DEFAULT="Europe/London"
+export BASE_IMG_URL=http://cdimage.ubuntu.com/releases/18.04.4/release/ubuntu-18.04.4-preinstalled-server-armhf+raspi3.img.xz
+ 
+export TARGET_HOSTNAME="leo"
+export FIRST_USER_NAME="pi"
+export FIRST_USER_PASS="raspberry"
+ 
+export LOCALE_DEFAULT="en_GB.UTF-8"
+export KEYBOARD_KEYMAP="gb"
+export KEYBOARD_LAYOUT="English (UK)"
+export TIMEZONE_DEFAULT="Europe/London"
