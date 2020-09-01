60a9d45936e6d62d3116a42acb17a5ce08578a09
Błażej Sowa <bsowa123@gmail.com>
2020-09-01 14:11:33 +0000

    Update the Ubuntu release

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ IMG_VERSION="0.1-ERC"
 export GIT_REPO="https://github.com/LeoRover/leo_os"
 export GIT_HASH="$(git rev-parse HEAD)"
 
-export BASE_IMG_URL=http://cdimage.ubuntu.com/releases/18.04.4/release/ubuntu-18.04.4-preinstalled-server-armhf+raspi3.img.xz
+export BASE_IMG_URL=http://cdimage.ubuntu.com/releases/18.04.5/release/ubuntu-18.04.5-preinstalled-server-armhf+raspi3.img.xz
  
 export TARGET_HOSTNAME="leo"
 export FIRST_USER_NAME="pi"
