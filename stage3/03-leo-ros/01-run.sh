7e5b114d86acf76aa17e05473601250f7d5427b8
Błażej Sowa <bsowa123@gmail.com>
2020-10-01 17:11:15 +0200

    Use the ros system user instead of root user for starting ROS nodes

--- stage3/03-leo-ros/01-run.sh
+++ stage3/03-leo-ros/01-run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
 
+on_chroot << EOF
+adduser ros --system --group --home /var/ros
+chmod 775 /var/ros
+for GRP in dialout audio video input gpio spi i2c; do
+  adduser ros "\${GRP}"
+done
+adduser $FIRST_USER_NAME ros
+EOF
+
 mkdir -p -m 775 "${ROOTFS_DIR}/etc/ros/urdf"
-mkdir -p -m 775 "${ROOTFS_DIR}/var/ros/"
 
 install -v -m 664 files/setup.bash "${ROOTFS_DIR}/etc/ros/"
 install -v -m 664 files/robot.launch "${ROOTFS_DIR}/etc/ros/"
@@ -11,10 +19,7 @@ install -v -m 755 files/leo-stop "${ROOTFS_DIR}/usr/bin/"
 install -v -m 644 files/leo.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 on_chroot << EOF
-groupadd -f -r ros
-adduser $FIRST_USER_NAME ros
 chown root:ros -R "/etc/ros"
-chown root:ros "/var/ros"
 systemctl enable leo
 EOF
 
