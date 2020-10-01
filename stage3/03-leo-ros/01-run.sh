28eabf6d67f604ae268b91c56ae4c2c82c8f2e8d
Błażej Sowa <bsowa123@gmail.com>
2020-10-01 19:56:30 +0200

    Use the default user instead of ros user for starting ROS nodes

--- stage3/03-leo-ros/01-run.sh
+++ stage3/03-leo-ros/01-run.sh
@@ -1,25 +1,22 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-adduser ros --system --group --home /var/ros
-chmod 775 /var/ros
-for GRP in dialout audio video input gpio spi i2c; do
-  adduser ros "\${GRP}"
-done
-adduser $FIRST_USER_NAME ros
-EOF
+mkdir -p -m 755 "${ROOTFS_DIR}/etc/ros/urdf"
+mkdir -p -m 755 "${ROOTFS_DIR}/var/ros/"
 
-mkdir -p -m 775 "${ROOTFS_DIR}/etc/ros/urdf"
-
-install -v -m 664 files/setup.bash "${ROOTFS_DIR}/etc/ros/"
-install -v -m 664 files/robot.launch "${ROOTFS_DIR}/etc/ros/"
-install -v -m 664 files/robot.urdf.xacro "${ROOTFS_DIR}/etc/ros/urdf/"
+install -v -m 644 files/setup.bash "${ROOTFS_DIR}/etc/ros/"
+install -v -m 644 files/robot.launch "${ROOTFS_DIR}/etc/ros/"
+install -v -m 644 files/robot.urdf.xacro "${ROOTFS_DIR}/etc/ros/urdf/"
 install -v -m 755 files/leo-start "${ROOTFS_DIR}/usr/bin/"
 install -v -m 755 files/leo-stop "${ROOTFS_DIR}/usr/bin/"
 install -v -m 644 files/leo.service "${ROOTFS_DIR}/etc/systemd/system/"
 
+install -v -m 644 files/tmux.conf "${ROOTFS_DIR}/etc/"
+
+sed -i "s|USER|${FIRST_USER_NAME}|" "${ROOTFS_DIR}/etc/systemd/system/leo.service"
+
 on_chroot << EOF
-chown root:ros -R "/etc/ros"
+chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} -R "/etc/ros"
+chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} "/var/ros"
 systemctl enable leo
 EOF
 
