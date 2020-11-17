3ed1d5f82cf6abeefcce992753b806ed5973b380
Błażej Sowa <bsowa123@gmail.com>
2020-11-17 20:09:23 +0000

    Change the default wallpaper

--- stage4/00-desktop-environment/01-run.sh
+++ stage4/00-desktop-environment/01-run.sh
@@ -3,3 +3,5 @@
 install -v -m 644 files/autologin.conf "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/"
 
 sed -i "s|USERNAME|${FIRST_USER_NAME}|" "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/autologin.conf"
+
+install -v -m 644 files/leo-mars-wallpaper.jpg "${ROOTFS_DIR}/usr/share/lubuntu/wallpapers/"
