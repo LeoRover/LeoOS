0dd3b1387a77f0961d72ce3e5c60d219b4bb97b2
Błażej Sowa <bsowa123@gmail.com>
2020-11-20 14:45:42 +0000

    Reorganize stage1, add polkit rule to allow sudo users to reboot and shutdown

--- stage1/04-net-tweaks/02-run.sh
+++ stage1/04-net-tweaks/02-run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 
+log "Configuring netplan"
+
 install -v -m 644 files/netplan/*.yaml "${ROOTFS_DIR}/etc/netplan/"
-install -v -m 644 files/polkit/*.pkla "${ROOTFS_DIR}/etc/polkit-1/localauthority/50-local.d/"
+
+
+log "Setting hostname"
 
 echo "${TARGET_HOSTNAME}" > "${ROOTFS_DIR}/etc/hostname"
 
 if ! grep -q "127.0.1.1 ${TARGET_HOSTNAME}" "${ROOTFS_DIR}/etc/hosts"; then
     printf "\n127.0.1.1 ${TARGET_HOSTNAME}\n" >> "${ROOTFS_DIR}/etc/hosts"
 fi
-
-install -v -m 644 files/regenerate_ssh_host_keys.service "${ROOTFS_DIR}/etc/systemd/system/"
-
-on_chroot << EOF
-systemctl enable regenerate_ssh_host_keys
-EOF
--- stage1/05-sys-tweaks/01-run.sh
+++ stage1/05-sys-tweaks/01-run.sh
@@ -20,11 +20,22 @@ chmod -v 440 "${ROOTFS_DIR}/etc/sudoers.d/010_${FIRST_USER_NAME}-nopasswd"
 
 on_chroot "systemctl disable ModemManager"
 
-install -v -m 644 files/udev.d/99-com.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
-install -v -m 644 files/sysctl.d/99-rpi.conf "${ROOTFS_DIR}/etc/sysctl.d/"
+
+log "Adding Fictionlab Apt repository"
 
 install -v -m 644 files/apt/fictionlab.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
-install -v -m 644 files/apt/fictionlab "${ROOTFS_DIR}/etc/apt/preferences.d/"
 
 on_chroot "apt-key add -" <files/apt/fictionlab.key
 on_chroot "apt-get update"
+
+
+log "Performing other system modifications"
+
+install -v -m 644 files/apt/fictionlab "${ROOTFS_DIR}/etc/apt/preferences.d/"
+install -v -m 644 files/udev.d/99-com.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
+install -v -m 644 files/sysctl.d/99-rpi.conf "${ROOTFS_DIR}/etc/sysctl.d/"
+install -v -m 644 files/polkit/*.pkla "${ROOTFS_DIR}/etc/polkit-1/localauthority/50-local.d/"
+
+install -v -m 644 files/regenerate_ssh_host_keys.service "${ROOTFS_DIR}/etc/systemd/system/"
+
+on_chroot "systemctl enable regenerate_ssh_host_keys"
