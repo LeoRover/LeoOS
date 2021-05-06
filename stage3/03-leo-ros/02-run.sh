1d641fb01a6b81b3206cadc4659ab5194ac7c65a
Błażej Sowa <bsowa123@gmail.com>
2021-05-06 20:00:05 +0200

    Use new packages for Leo ERC

--- stage3/03-leo-ros/02-run.sh
+++ stage3/03-leo-ros/02-run.sh
@@ -6,6 +6,9 @@ install -m 644 files/leo-erc.repos "${ROOTFS_DIR}/etc/ros/catkin_ws/"
 on_chroot <<EOF
 cd /etc/ros/catkin_ws
 vcs import < leo-erc.repos
+rosdep update
+rosdep fix-permissions
+rosdep install --from-paths src -iy
 catkin config --extend /opt/ros/melodic --install -i /opt/ros/leo
 catkin build
 EOF
