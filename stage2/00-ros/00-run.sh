0b5ad4c8c399f7a5c83fbed7a1dc61a80f869f79
Błażej Sowa <bsowa123@gmail.com>
2021-06-01 15:05:56 +0000

    Download the newest rosdistro GPG key

--- stage2/00-ros/00-run.sh
+++ stage2/00-ros/00-run.sh
@@ -2,6 +2,7 @@
 
 install -v -m 644 files/ros-latest.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
-on_chroot apt-key add - < files/ros.key
-
-on_chroot "apt-get update"
+on_chroot << EOF
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+apt-get update
+EOF
