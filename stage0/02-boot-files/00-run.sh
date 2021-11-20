6d5d776c1ba2e847253b3cb663253d99d3f6ef70
Błażej Sowa <bsowa123@gmail.com>
2021-11-20 00:57:54 +0000

    Install u-boot binaries from Fictionlab Archive

--- stage0/02-boot-files/00-run.sh
+++ stage0/02-boot-files/00-run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 install -v -m 644 files/firmware/*.txt "${ROOTFS_DIR}/boot/firmware/"
-install -v -m 644 files/firmware/*.bin "${ROOTFS_DIR}/boot/firmware/"
 install -v -m 644 files/firmware/README "${ROOTFS_DIR}/boot/firmware/"
 
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
