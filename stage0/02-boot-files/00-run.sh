7349703640c82dc20ffcca4cb2896b1b0e11c89f
Błażej Sowa <bsowa123@gmail.com>
2021-11-23 19:22:27 +0100

    Copy u-boot binaries to /boot/firmware

--- stage0/02-boot-files/00-run.sh
+++ stage0/02-boot-files/00-run.sh
@@ -4,3 +4,8 @@ install -v -m 644 files/firmware/*.txt "${ROOTFS_DIR}/boot/firmware/"
 install -v -m 644 files/firmware/README "${ROOTFS_DIR}/boot/firmware/"
 
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
+
+for uboot_binary in ${ROOTFS_DIR}/usr/lib/u-boot/*/u-boot.bin; do
+    dest_binary=uboot_$(basename $(dirname "${uboot_binary}")).bin
+    cp -v "${uboot_binary}" "${ROOTFS_DIR}/boot/firmware/${dest_binary}"
+done
