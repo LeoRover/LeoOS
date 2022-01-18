d8be587034e3a91942e74e320c59198fcd9c6a7e
Błażej Sowa <bsowa123@gmail.com>
2022-01-18 20:25:55 +0100

    Set connections editor in nm-tray

--- stage4/00-desktop-environment/01-run.sh
+++ stage4/00-desktop-environment/01-run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 
-install -v -m 644 files/autologin.conf "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/"
+install -v -m 644 files/lightdm/autologin.conf "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/"
 
 sed -i "s|USERNAME|${FIRST_USER_NAME}|" "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/autologin.conf"
 
-mkdir -p "${ROOTFS_DIR}/usr/share/themes/leo"
+mkdir -v -p "${ROOTFS_DIR}/usr/share/themes/leo"
 install -v -m 644 files/leo-mars-wallpaper.jpg "${ROOTFS_DIR}/usr/share/themes/leo/"
+
+mkdir -v -p "${ROOTFS_DIR}/etc/xdg/nm-tray"
+install -v -m 644 files/nm-tray/nm-tray.conf "${ROOTFS_DIR}/etc/xdg/nm-tray/"
+
