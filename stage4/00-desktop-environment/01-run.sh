dace5ae0ed947dd7a86b9db00d3b871f980ae7a2
Błażej Sowa <bsowa123@gmail.com>
2022-04-07 02:05:38 +0200

    Disable lightdm autologin

--- stage4/00-desktop-environment/01-run.sh
+++ stage4/00-desktop-environment/01-run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-install -v -m 644 files/lightdm/autologin.conf "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/"
-
-sed -i "s|USERNAME|${FIRST_USER_NAME}|" "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/autologin.conf"
-
 mkdir -v -p "${ROOTFS_DIR}/usr/share/themes/leo"
 install -v -m 644 files/leo-mars-wallpaper.jpg "${ROOTFS_DIR}/usr/share/themes/leo/"
 
