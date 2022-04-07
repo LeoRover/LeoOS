e4a3403141a010f61aeaaec6afacd5e051f7f152
Błażej Sowa <bsowa123@gmail.com>
2022-04-07 15:01:20 +0000

    Add lightdm greeter config

--- stage4/00-desktop-environment/01-run.sh
+++ stage4/00-desktop-environment/01-run.sh
@@ -7,3 +7,4 @@ mkdir -v -p "${ROOTFS_DIR}/etc/xdg/nm-tray"
 install -v -m 644 files/nm-tray/nm-tray.conf "${ROOTFS_DIR}/etc/xdg/nm-tray/"
 
 sed -i "s|user = CHANGE_ME|user = ${FIRST_USER_NAME}|" "${ROOTFS_DIR}/etc/lightdm/lightdm-mini-greeter.conf"
+install -v -m 644 files/lightdm/greeter.conf "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/"
