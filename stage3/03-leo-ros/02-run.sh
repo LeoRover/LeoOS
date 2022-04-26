03c84ab1faff29ea6be347dedaf4b05e5dd83045
Błażej Sowa <bsowa123@gmail.com>
2022-04-26 18:52:09 +0200

    Add script for updating leo_erc packages

--- stage3/03-leo-ros/02-run.sh
+++ stage3/03-leo-ros/02-run.sh
@@ -2,3 +2,9 @@
 
 mkdir -p "${ROOTFS_DIR}/etc/ros/catkin_ws"
 install -m 644 files/leo-erc.repos "${ROOTFS_DIR}/etc/ros/catkin_ws/"
+install -m 755 files/update.sh "${ROOTFS_DIR}/etc/ros/catkin_ws/"
+
+on_chroot <<EOF
+su ${FIRST_USER_NAME}
+/etc/ros/catkin_ws/update.sh
+EOF
---
+++ stage3/03-leo-ros/files/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ROS_HOME=/var/ros
+
+cd /etc/ros/catkin_ws
+vcs import < leo-erc.repos
+rosdep update
+rosdep install --rosdistro noetic --from-paths src -iy
+catkin config --extend /opt/ros/noetic --install -i /opt/ros/leo
+catkin build --no-status
