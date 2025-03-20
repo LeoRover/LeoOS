cdaf9666a0abd55f615a53ecaaf85c31075b4501
Błażej Sowa <bsowa123@gmail.com>
2025-03-20 09:29:03 +0100

    Don't override non-existent input

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
