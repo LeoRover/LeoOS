60657976bfedfb957557bf21d03cc56cda245201
Błażej Sowa <bsowa123@gmail.com>
2025-05-06 19:52:46 +0200

    Remove logs and backup files

--- OS-image/scripts/buildStageFinal.sh
+++ OS-image/scripts/buildStageFinal.sh
@@ -7,9 +7,20 @@ mkdir /mnt
 mount -t ext4 "$DISK"2 /mnt
 mount -t vfat "$DISK"1 /mnt/boot/firmware
 
+# Set the OS name, version, and variant
 sed -i "s|@OS_NAME@|${OSName}|g" /mnt/etc/custom-os-release
 sed -i "s|@OS_VERSION@|${OSVersion}|g" /mnt/etc/custom-os-release
 sed -i "s|@OS_VARIANT@|${OSVariant}|g" /mnt/etc/custom-os-release
+
+# Remove backup files
+find "/mnt/etc" -type f -name "*-" -exec rm -v {} \;
+find "/mnt/var" -type f -name "*-old" -exec rm -v {} \;
+
+# Truncate all logs
+find "/mnt/var/log/" -type f -exec cp -v /dev/null {} \;
+
+# Clear up /run directory
+rm -v -rf "/mnt/run/"*
 
 # Unmount everything
 umount /mnt/boot/firmware
