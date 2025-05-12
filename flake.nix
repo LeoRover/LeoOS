8e8df9f4e93440c4285a326b16e7368a202859c5
Błażej Sowa <bsowa123@gmail.com>
2025-05-12 19:08:03 +0200

    2.0.0

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         pkgs = (import nixpkgs) { inherit system; };
 
         OSName = "LeoOS";
-        OSVersion = "2.0.0beta1";
+        OSVersion = "2.0.0";
 
         OSImageDerivations = pkgs.callPackage ./OS-image {
           inherit OSName OSVersion;
