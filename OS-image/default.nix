92046d7f0d190771c82f70b937929f46460582b6
Jan Hernas <hernasj@wp.pl>
2026-05-13 11:51:10 +0200

    Add ROS aruco opencv packages

--- OS-image/default.nix
+++ OS-image/default.nix
@@ -207,6 +207,8 @@ let
       "ros-jazzy-leo-camera" # hidden dependency of leo_robot
       "ros-jazzy-compressed-image-transport" # image transport plugin that provides compressed image streams
       "ros-jazzy-micro-ros-agent" # For talking with LeoCore
+      "ros-jazzy-aruco-opencv-msgs" # ROS messages for aruco tracking
+      "ros-jazzy-aruco-opencv" # For aruco tracking
 
       "---"
 
